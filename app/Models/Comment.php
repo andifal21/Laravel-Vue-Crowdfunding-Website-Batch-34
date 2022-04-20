61ffdb61123c63a63a0019a772a6f25c95897caa
Andi <naufalandi007@gmail.com>
2022-04-21 00:27:55 +0800

    first commit

---
+++ app/Models/Comment.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use App\Traits\UsesUuid;
+
+class Comment extends Model
+{
+    use HasFactory;
+    Use UsesUuid;
+    protected $guarded = [];
+}
